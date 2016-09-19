82b48126862314bf0e740accd6db4f3c0b5deed3
George Bonner <georgebonnr@gmail.com>
2016-09-19 12:12:54 -0700

    url builder

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import isPlainObject from 'is-plain-object'
+
+
+const _encode = value => Array.isArray(value) && !value.length ? '[]' : encodeURI(value)
+
+const encodeParam = (value, key) => {
+    if (isPlainObject(value)) {
+        return Object.keys(value).reduce((memo, _key) => {
+            return `${memo}${key}[${_key}]=${_encode(value[_key])}&`
+        }, '')
+    } else {
+        return `${key}=${_encode(value)}&`
+    }
+}
+
+export const encodeParams = (params = {}) => {
+    if (!isPlainObject(params)) throw new Error('encodeParams expects a plain object')
+    return Object.keys(params).reduce((memo, key) =>
+        `${memo}${encodeParam(params[key], key)}`
+    , '').slice(0,-1)
+}
+
+
+export const createUrlBuilder = (baseUrl = '', baseParams = {}) =>
+    (relativeUrl, params = {}) => {
+        if (relativeUrl.charAt(0) !== '/') {
+            relativeUrl = `/${relativeUrl}`
+        }
+        const separator = relativeUrl.includes('?') ? '&' : '?'
+        // doing this because baseParams should be overridden by params,
+        // but preferable to havebaseParams at end of querystring
+        // (even if key order not fully guaranteed)
+        params = Object.keys(baseParams).reduce((memo, k, v) => {
+            if (memo[k]) return memo
+            memo[k] = v
+            return memo
+        }, {...params})
+
+        return `${baseUrl}${relativeUrl}${separator}${encodeParams(params)}`
+    }
